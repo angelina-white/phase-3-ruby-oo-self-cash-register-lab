314cf0794b08ddff87ccb5ec06cec8fe7712ca57
Angelina <angelinarwhite@gmail.com>
2022-06-11 14:46:45 -0400

    commit tree fe7f36748e76b7070fd493b628ea79a3140e4545

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister
 
+    attr_reader :employeeDiscount
+    attr_accessor :total
+
+    def initialize(total=0, employeeDiscount=0)
+        @total=total
+        @employeeDiscount=employeeDiscount
+    end
+
+end
